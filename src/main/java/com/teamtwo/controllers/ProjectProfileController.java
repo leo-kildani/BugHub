4e81caa6412336cb4d5ee34b5202f4064730cd4d
Leonardo Kildani <leonardo.kildani@sjsu.edu>
2023-10-24 18:57:43 -0700

    implement project descr editing feature

--- src/main/java/com/teamtwo/controllers/ProjectProfileController.java
+++ src/main/java/com/teamtwo/controllers/ProjectProfileController.java
@@ -4,16 +4,21 @@ import com.teamtwo.data_model.BugHubDataModel;
 import com.teamtwo.entity.Project;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
+import java.net.URL;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.ResourceBundle;
 
-public class ProjectProfileController implements BugHubController {
+public class ProjectProfileController implements BugHubController, Initializable {
 
     @FXML
     private Label projectTitle;
@@ -22,14 +27,34 @@ public class ProjectProfileController implements BugHubController {
     private Label projectDate;
 
     @FXML
-    private Button openTicketButton;
+    private Label characterCount;
 
     @FXML
-    private Button deleteTicketButton;
+    private TextArea projectDescr;
 
+    @FXML
+    private Label savingDescrHelpLabel;
     private Project project;
 
     private BugHubDataModel model;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        projectDescr.setOnMouseClicked(mouseEvent -> {
+            if (mouseEvent.getButton().equals(MouseButton.PRIMARY) && mouseEvent.getClickCount() == 2) {
+                savingDescrHelpLabel.setVisible(true);
+                projectDescr.setEditable(true);
+            }
+        });
+        projectDescr.addEventFilter(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            if (mouseEvent.getButton().equals(MouseButton.PRIMARY) && projectDescr.isEditable()) {
+                savingDescrHelpLabel.setVisible(false);
+                projectDescr.setEditable(false);
+                characterCount.setText(projectDescr.getText().length() + "/256");
+                project.setDescr(projectDescr.getText());
+            }
+        });
+    }
 
     @Override
     public void loadModel(BugHubDataModel model) {
@@ -51,6 +76,8 @@ public class ProjectProfileController implements BugHubController {
         this.project = project;
         this.projectTitle.setText(project.getTitle());
         this.projectDate.setText("Date Created: " + project.getDate().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)));
+        this.projectDescr.setText(project.getDescr());
+        this.characterCount.setText(project.getDescr().length() + "/256");
     }
 
     public Project getProject() {
