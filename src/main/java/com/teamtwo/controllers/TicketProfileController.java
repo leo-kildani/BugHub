0e2f6cd6e36036478629df012d745d0240bd42e9
Leonardo Kildani <leonardo.kildani@sjsu.edu>
2023-11-03 15:04:44 -0700

    more fixes

--- src/main/java/com/teamtwo/controllers/TicketProfileController.java
+++ src/main/java/com/teamtwo/controllers/TicketProfileController.java
@@ -158,5 +158,6 @@ public class TicketProfileController extends AbstractBugHubController implements
 				mouseEvent.consume();
 			}
 		});
+		commentList.setPlaceholder(new Label("Create new comment under \"New Comment\" section!"));
 	}
 }
